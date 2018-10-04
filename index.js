33a204ea084141ff822370c81a42e0a908bab05a
GSchreiberPDX <gschreiber000@gmail.com>
2018-10-03 20:43:06 -0700

    loop test

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('hello bcpreppers!');
+var countTo = prompt('how high should we count even numbers?')
+
+var sure = confirm('are ou sure you want to count to ' + countTo)
+
+for(var i = 0; i < 100; i++) {
+    var y = i % 2;
+    if (y === 0) {
+        console.log(i, 'is even');
+    }
+}
+
